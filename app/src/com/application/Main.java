af83cbf6e79471b8ad3341e00d5ba9ad02c98061
Duc Nguyen <nthduc95@gmail.com>
2024-04-15 17:05:45 +0700

    Implement build docker with maven

--- app/src/com/application/Main.java
+++ app/src/com/application/Main.java
@@ -1,7 +1,12 @@
 package com.application;
 
+import com.application.model.Customer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Customer customer = new Customer("first", "second", "j@domain.com");      
+        System.out.println(customer);         
+        Customer customer1 = new Customer("first", "second", "email");
+        System.out.println(customer1);
     }
 }
